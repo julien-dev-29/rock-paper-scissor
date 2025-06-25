83bf4a5943c2ec2607ba108393b774bd25f37923
Jurol <jurol@example.com>
2025-06-25 11:24:15 +0200

    Add ui to the game

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 let userScore = 0
 let computerScore = 0
-
+var resultDiv = document.querySelector('.result')
+var userScoreDiv = document.querySelector('.user-score')
+var computerScoreDiv = document.querySelector('.computer-score')
+userScoreDiv.textContent = userScore
+computerScoreDiv.textContent = computerScore
 /**
  * Retourne le choix du computer
  * 
  * @returns {String}
  */
 function getComputerChoice() {
-    const randomNumber = Math.random()
-    if (randomNumber < 1 / 3) {
-        return "Rock"
-    }
-    if (randomNumber < 2 / 3 && randomNumber > 1 / 3) {
-        return "Paper"
-    }
-    if (randomNumber < 1 && randomNumber > 2 / 3) {
-        return "Scissors"
+    const randomNumber = Math.floor(Math.random() * 3)
+    console.log(randomNumber);
+    switch (randomNumber) {
+        case 0:
+            return "Rock"
+        case 1:
+            return "Paper"
+        case 2:
+            return "Scissors"
+        default:
+            break;
     }
 }
 
-/**
- * Retourne le choix du joueur
- * 
- * @returns {String}
- */
-function getUserChoice() {
-    return prompt('Rock, Paper, Scissors ?')
-}
+// /**
+//  * Retourne le choix du joueur
+//  * 
+//  * @returns {String}
+//  */
+// function getUserChoice() {
+//     return prompt('Rock, Paper, Scissors ?')
+// }
 
 /**
  * Joue un simple round
@@ -38,36 +44,52 @@ function playRound(userChoice, computerChoice) {
     userChoice = userChoice.toLowerCase()
     computerChoice = computerChoice.toLowerCase()
 
+    if (userChoice === computerChoice) return "Ex aequo!!!"
+
     if (userChoice === 'rock' && computerChoice === 'scissors' ||
         userChoice === 'paper' && computerChoice === 'rock' ||
         userChoice === 'scissors' && computerChoice === 'paper'
     ) {
-        console.log(`You win! ${userChoice} beats ${computerChoice}`);
-        ++userScore
+        userScore++
+        return `You win! ${userChoice} beats ${computerChoice}`
     }
+
     if (userChoice === 'rock' && computerChoice === 'paper' ||
         userChoice === 'paper' && computerChoice === 'scissors' ||
         userChoice === 'scissors' && computerChoice === 'rock'
     ) {
-        console.log(`You lose! ${computerChoice} beats ${userChoice}`);
-        ++computerScore
+        computerScore++
+        return `You lose! ${computerChoice} beats ${userChoice}`
     }
 }
 
-/**
- * Joue 5 rounds
- */
-function playGame() {
-    for (let index = 0; index < 5; index++) {
-        const userSelection = getUserChoice()
-        const computerSelection = getComputerChoice()
-        playRound(userSelection, computerSelection)
-    }
-    if (userScore > computerScore) {
-        console.log("You win the game!");
-    } else {
-        console.log("You lose the game!");
-    }
+function displayResult(e) {
+    e.stopPropagation()
+    var userSelection = e.target.textContent
+    var computerSelection = getComputerChoice()
+    resultDiv.textContent = playRound(userSelection, computerSelection)
+    updateScore()
 }
 
-playGame()
+function initScore() {
+    userScore = 0
+    computerScore = 0
+}
+
+function updateScore() {
+    if (userScore === 5) {
+        resultDiv.style.color = "#00D390"
+        resultDiv.style.fontSize = "42px"
+        resultDiv.textContent = "You win the game!"
+    }
+    if (computerScore === 5) {
+        resultDiv.style.color = "#FF627D"
+        resultDiv.style.fontSize = "42px"
+        resultDiv.textContent = "You lose the game!"
+    }
+    userScoreDiv.textContent = userScore
+    computerScoreDiv.textContent = computerScore
+}
+
+const buttons = document.querySelectorAll('.btn')
+buttons.forEach(button => button.addEventListener('click', displayResult))
